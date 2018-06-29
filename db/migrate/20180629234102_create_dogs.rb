3644ae0aef026dfa6af5e0afe73396f1270214ea
taryn yaeger <tsyaeger@gmail.com>
2018-06-29 23:47:32 +0000

    automatically backed up by learn

--- db/migrate/20180629234102_create_dogs.rb
+++ db/migrate/20180629234102_create_dogs.rb
@@ -1,18 +1,24 @@
 class CreateDogs < ActiveRecord::Migration
   
-  def up
-    create_table :dogs do |t|
-    t.string :name 
-    t.string :breed
-    end
-  end
+  # def up
+  #   create_table :dogs do |t|
+  #   t.string :name 
+  #   t.string :breed
+  #   end
+  # end
   
-  def down
-    drop_table :dogs
-  end
+  # def down
+  #   drop_table :dogs
+  # end
   
   
   
   def change
+    create_table :dogs do |t|
+      t.string :name 
+      t.string :breed 
+    end 
   end
+  
+  
 end
